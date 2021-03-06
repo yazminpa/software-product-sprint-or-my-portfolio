c3c8f107f83e45343f1f9ebda3daeb3059379e5f
Yazmin Padilla <yazminpa@stanford.edu>
2021-03-06 23:05:33 +0000

    Add selfie image, written paragraph, and colors to main page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Youre a Gryffindor!', '¡Youre a Ravenclaw!', 'Youre a Slytherin', 'Youre a Hufflepuff!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
